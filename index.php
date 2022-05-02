b72b0d4b52c3c21ce82243abea4b36e4b757eb43
ololodo <gthtekj@mail.ru>
2022-05-02 11:48:18 +0000

    update index.php again

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php //encoding UTF-8
-session_start();
-$sid=session_id();
 function s_add($x,$y){
     return($x+$y);
 };
+session_start();
+$sid=session_id();
+echo(sprintf("%s+%s=%s\n",1,1,s_add(1,1)));
 //
 ?>
